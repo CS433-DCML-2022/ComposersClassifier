8f2a6dc413b61f7d808ff55427e24a2af41779fc
johentsch <johannes.hentschel@epfl.ch>
2022-11-30 19:22:39 +0100

    adapts jsonlExtract.py to extract only JSON for present files

--- data/jsonlExtract.py
+++ data/jsonlExtract.py
@@ -40,20 +40,32 @@ def remove_dunder(d):
 def main(args):
 
     target = args.output
-
+    if args.filter is not None:
+        filtering = True
+        filenames = [os.path.splitext(file)[0] for file in os.listdir(args.filter)]
+    else:
+        filtering = False
 
     with open(args.file) as j:
         json_list = list(j)
         for json_str in json_list:
-            result = loads(json_str)
+            try:
+                result = loads(json_str)
+            except Exception:
+                print(f"Could not read this line as JSON:\n{json_str}")
+                raise
             fname = result.get('id')
             #handle empty json
             if fname == None:
                 print("Error for json: " + str(result))
                 continue
+            if filtering and fname not in filenames:
+                if args.verbose:
+                    print(f"Excluding ID {fname}.")
+                continue
             curr = os.path.join(target, fname + '.json')
             if args.verbose:
-                print(curr)
+                print(f"Writing {curr}")
             with open (curr, 'w', newline='') as jsonFile:
                 dump(result,jsonFile)
 
@@ -63,6 +75,8 @@ if __name__ == '__main__':
     parser.add_argument('file',metavar='JSONL_FILE', help='Path to the JSONL file to be converted.')
     parser.add_argument('-o', '--output', metavar='json_FILE_DIR', help='DIR for json files.')
     parser.add_argument('-v', '--verbose', action='store_true', help='Show more output.')
+    parser.add_argument('--filter', metavar='ID_FILES_DIR', help='''In case you only want to output JSON files for IDs for which a file exists, pass
+a directory containing files of which file names are IDs.''')
     # parser.add_argument('-d', '--dunder', action='store_false', help="Retain columns beginning with __ (dunder).")
     args = parser.parse_args()
     main(args)
