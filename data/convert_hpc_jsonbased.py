cea897f792e4f078d18f8f281d933054b5a4e340
johentsch <johannes.hentschel@epfl.ch>
2022-12-01 19:20:46 +0100

    factors out write_json() and changes order of steps

--- data/convert_hpc_jsonbased.py
+++ data/convert_hpc_jsonbased.py
@@ -1,4 +1,6 @@
-import os, time, subprocess
+import os, subprocess
+from typing import Optional
+
 import ms3
 import ray
 import json
@@ -18,7 +20,7 @@ MUSESCORE_CMD = ms3.get_musescore("auto")
 def make_id2path_dict(path):
     return {os.path.splitext(entry.name)[0]: entry.path for entry in os.scandir(path) if entry.is_file()}
 
-NB_THREADS = 2
+NB_THREADS = 12
 FULL_METADATA = True
 JSON_FILES = make_id2path_dict(JSON_FOLDER)
 MSCZ_FILES = make_id2path_dict(MSCZ_FOLDER)
@@ -36,19 +38,30 @@ for dir in OUTPUT_PATHS.values():
 
 
 @ray.remote
-def process_chunk(low, high):
+def process_chunk(low: int, high: int) -> None:
     # print("Instance received files to work on : ", JSON_FILENAMES[low:high])
     for i in range(low, high):
         ID = ALL_IDS[i]
         json_file = JSON_FILES[ID]
         with open(json_file, "r", encoding='utf-8') as f:
             jsondict = json.load(f)
+        if "__terminated__" in jsondict :
+            continue
         mscz_file = MSCZ_FILES[ID]
         converted_mscz_file = os.path.join(OUTPUT_PATHS["conversion"], ID + ".mscz")
         json_outfile = os.path.join(OUTPUT_PATHS['metadata'], ID + ".json")
 
-        if "__terminated__" in jsondict :
-            continue
+        def write_json(jsondict: dict, error: Optional[str] = None):
+            nonlocal json_outfile
+            jsondict['__terminated__'] = True
+            if '__error__' not in jsondict:
+                jsondict['__error__'] = []
+            if error is not None:
+                jsondict['__error__'].append(str(error))
+                jsondict['__has_error__'] = True
+            with open(json_outfile, "w", encoding='utf-8') as f:
+                json.dump(jsondict, f)
+
         score_meta=[]
         try:
             convert = subprocess.run(
@@ -58,32 +71,16 @@ def process_chunk(low, high):
             )
             if convert.returncode != 0:
                 raise Exception(convert.stderr)
-            if FULL_METADATA:
-                score_meta = subprocess.run(
-                    [MUSESCORE_CMD, "--score-meta", converted_mscz_file],
-                    capture_output=True,
-                    text=True,
-                )
-                if score_meta.returncode != 0:
-                    raise Exception(score_meta.stderr)
             parsed = ms3.Score()
             with ms3.capture_parse_logs(parsed.logger, level='i') as capturer:
                 parsed.parse_mscx(converted_mscz_file, read_only=True)
                 log_messages = capturer.content_list
         except KeyboardInterrupt:
-            raise KeyboardInterrupt
+            raise
         except Exception as e:
-            if '__error__' in jsondict:
-                jsondict['__error__'].append(str(e))
-            else:
-                jsondict['__error__']=[str(e)]
-
-            jsondict['__has_error__'] = True
-            jsondict['__terminated__'] = True
-            with open(json_outfile, "w", encoding='utf-8') as f:
-                json.dump(jsondict, f)
+            write_json(jsondict, error=e)
             continue
-
+        jsondict['ms3_metadata'] = parsed.mscx.metadata
         zip_features_file = os.path.join(OUTPUT_PATHS["features"], f"{ID}.zip")
         if os.path.isfile(zip_features_file):
             os.remove(zip_features_file)
@@ -101,22 +98,19 @@ def process_chunk(low, high):
         with ZipFile(zip_features_file, 'a') as myzip:
             myzip.writestr('log.txt', '\n'.join(log_messages))
 
+        error = None
         if FULL_METADATA:
-            mscore_metadict = json.loads(score_meta.stdout)
-            jsondict['scoremeta_metadata'] = mscore_metadict
-        
-        # To change once no NATypes left
-        for k,v in parsed.mscx.metadata.items():
-          jsondict['ms3_data_'+str(k)]  = str(v)
-        # jsondict['ms3_data'] = parsed.mscx.metadata
-        
-        if not '__error__' in jsondict:
-            jsondict['__error__']=[]
-        jsondict['__has_error__'] = False
-        jsondict['__terminated__'] = True
-        with open(json_outfile, "w", encoding='utf-8') as f:
-            json.dump(jsondict, f)
-    return
+            score_meta = subprocess.run(
+                [MUSESCORE_CMD, "--score-meta", converted_mscz_file],
+                capture_output=True,
+                text=True,
+            )
+            if score_meta.returncode == 0:
+                jsondict['musescore_metadata'] = json.loads(score_meta.stdout)
+            else:
+                error = score_meta.stderr
+
+        write_json(jsondict, error)
 
 
 def main():
