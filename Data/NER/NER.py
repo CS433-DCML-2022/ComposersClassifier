5ea737b1969b3d64df74ed7d00611362f39204c5
jmic8397 <jmic8397@uni.sydney.edu.au>
2022-12-05 12:14:45 +0100

    Added multi language support, needs testing

--- Data/NER/NER.py
+++ Data/NER/NER.py
@@ -2,7 +2,7 @@ import spacy
 import os
 import json
 from functools import lru_cache
-
+from langdetect import detect
 
 COMPOSERDIR =  "composer_labels.txt"
 #Fields to query for composer data - "title"?, 
@@ -10,17 +10,45 @@ FIELDS = ["description", "title"]
 JSONDIR = "/Users/jamiemickaill/Desktop/ML_PROJECT_2/NERbranch/ComposersClassifier/Data/metadata"
 SKIPIFCOMPOSEREXISTS = True
 KNOWNCOMPOSERSONLY = True
-DEBUG = False
+SAVELANG = True
+MULTIPLELANGUAGES = True
+#All lang models require install on target system with python -m spacy download language_core_model_name
+ALTLANGS = {'de':["de_core_news_sm","de_dep_news_trf"], 'ko':["ko_core_news_sm","ko_core_news_lg"],'en':["en_core_web_sm","en_core_web_trf"],'ja':["ja_core_news_sm","ja_core_news_trf"],'ru':["ru_core_news_sm","ru_core_news_lg"],'it':["it_core_news_sm","it_core_news_lg"], 'es':["es_core_news_sm", "es_dep_new_trf"] }
+#if true will retrieve _trf (transformer based) language models instead of _sm
+# ~438mb file vs 12mb for en
+ACCURACYOVERSPEED = False
+DEBUG = True
+#To add - if composer has an english name use it to reduce variation
+CONVERTALLTOEN = False
 
 '''Draft Named Entity Recognition for retrieving possible composers from associated metadata'''
 
+#make composer list (compser,ID) and dict with ID -> [names]
 def loadComposers(file):
-    composers = list()
+    composersDict = dict()
+    composersList = list()
     with open(file,'r') as comps:
         for line in comps.readlines():
-            composers.append(line.split(',')[1].rstrip("\n"))
-    return composers
+            splitLine = line.split(',')
+            ID = splitLine[0].split('/')[-1]
+            composer = splitLine[1].strip('\n')
+            if composersDict.get(ID):composersDict[ID] = composersDict[ID].append(composer)
+            else: composersDict[ID] = list(composer)
+            composersList.append((composer,ID))
+    return composersDict,composersList
 
+def initModelDict():
+    modelDict = dict()
+    #init model/s
+    if MULTIPLELANGUAGES:
+        for lang in ALTLANGS.keys():
+            if ACCURACYOVERSPEED:
+                modelDict[lang]=spacy.load(ALTLANGS[lang][1])
+            else: modelDict[lang]=spacy.load(ALTLANGS[lang][0])
+    else:
+        if ACCURACYOVERSPEED: modelDict['en'] = spacy.load("en_core_web_rtf") 
+        else: modelDict['en'] = spacy.load("en_core_web_sm")
+    return modelDict
 
 #Lev dist calculation
 def lev_dist(a, b):
@@ -39,8 +67,13 @@ def lev_dist(a, b):
         )
     return min_dist(0, 0)
 
+def getENcomposerName(name,dict):
+    possibleNames = dict[name[1]]
+    possibleEnglishNames = [name for name in possibleNames if detect(name) == 'en' ] 
+    if len(possibleEnglishNames) == 0: return [name[0]]
+    return possibleEnglishNames
 
-def namedEntityRecognition(stringToParse, composerFile, NLPmodel):
+def namedEntityRecognition(stringToParse, composersDict,composersList, NLPmodel, lang):
     
     #remove special chars
     stringToParse = ''.join(e for e in stringToParse if e.isalnum() or e==" ")
@@ -50,32 +83,41 @@ def namedEntityRecognition(stringToParse, composerFile, NLPmodel):
     properNouns = [ent.text for ent in doc.ents ]
 
     #composerCheck
-    composers = loadComposers(composerFile)
-    possibleComposers = [[composer for composer in composers if prop in composer] for prop in properNouns]
+    possibleComposers = [[composer for composer in composersList if prop in composer[0]] for prop in properNouns]
+    
+    #remove empty and flatten
+    possibleComposers = [item for sublist in possibleComposers for item in sublist if item]
+
     #this will include all proper nouns from text 
     if not KNOWNCOMPOSERSONLY:
         possibleComposers += properNouns
 
     if DEBUG: print(possibleComposers)
+
     #take best / most similar candidate for each proper noun
     #Note: if not KNOWN composers only, then all proper nouns will be selected (due to similarity/distance measure used)
-    candidates = []
+    candidates = list()
     for prop in properNouns:
-        candidates.append(sorted(possibleComposers, key= lambda x: lev_dist(prop,x))[0])
+        candidates.append(sorted(possibleComposers, key= lambda x: lev_dist(prop,x[0]))[0])
+    
+    #convert to EN
+    if lang != 'en':
+        candidates = [ [enName for enName in getENcomposerName(candidate,composersDict)] for candidate in candidates]
 
     return candidates
 
-
-
 if __name__ == "__main__":
 
-    #init model/s
-    nlpEnglish = spacy.load("en_core_web_sm")
+    #init all language models
+    modelDict = initModelDict()
+    composersDict,composersList = loadComposers(COMPOSERDIR)
 
     #Iterate over all JSON in Dir
     for jsonFile in os.listdir(JSONDIR):
         if DEBUG: print(jsonFile)
         f = os.path.join(JSONDIR,jsonFile)
+
+        #read json
         with open(f,'r',newline='') as currJson:
             data = currJson.read()
             jsonObj = json.loads(data)
@@ -87,30 +129,44 @@ if __name__ == "__main__":
                     print(jsonFile + ": Composer currently exists = " + str(composer))
                     continue
 
-            #just description? set fields
+            #Query each specified field of the json for text
             currQueryText = []
             for field in FIELDS:
-                if (jsonObj.get(field)):
-                    #check if field contains .Arr?
-                    currQueryText.append(jsonObj.get(field))
+                #check if field contains .Arr? - to add
+                if (jsonObj.get(field)): currQueryText.append(jsonObj.get(field))
     
             if DEBUG: print(currQueryText)
+            currQueryText = str(" ".join(currQueryText))
 
             #any text to query with?
-            if currQueryText != []:
-                entities = namedEntityRecognition(str(" ".join(currQueryText)), COMPOSERDIR, nlpEnglish)
-            else:
-                continue
+            if len(currQueryText) > 0 :
 
-            #else try another dictionary/language to scrape entities
-            if entities == None:
-                #for language in languages...
+                #Detect language - maybe useful feature / can be used to select correct model
+                try: lang = detect(currQueryText) 
+                except: 
+                    if DEBUG: print("failed to detect language of query " + currQueryText)
+                    continue
+
+                if DEBUG: print(lang)
+                if SAVELANG: jsonObj["Language"] = lang
+
+                #if we have the language - try get the proper nouns
+                if modelDict.get(lang):
+                    entities = namedEntityRecognition(currQueryText, composersDict,composersList, modelDict.get(lang), lang)
+                
+                #if we dont have language - we cant recognize the proper nouns
+                # save new json including language and quit?
+                else: 
+                    if SAVELANG: json.dump(jsonObj,jsonFile)
+                    if DEBUG: print("language model not available for query" + currQueryText + " in lang " + lang)
+                    continue
+            else:
                 continue
 
             #Write composer to entities
             #what to do if multiple? select first only
-            jsonObj["Composer"] = str(entities[0])
-            if DEBUG: print(entities[0])
+            jsonObj["Composer"] = str(entities[0][0])
+            if DEBUG: print(entities[0][0])
 
         with open (f, 'w', newline='') as jsonFile:
             json.dump(jsonObj,jsonFile)
