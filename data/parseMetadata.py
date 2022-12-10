05db899a53074d7be59ac3ca6ab86458ecf5e514
jmic8397 <jmic8397@uni.sydney.edu.au>
2022-12-11 00:09:35 +0100

    Began processing alternative text fields with NER

--- data/parseMetadata.py
+++ data/parseMetadata.py
@@ -2,7 +2,7 @@ import csv
 import os
 import argparse
 import sys
-from ray_NER import basic_clean
+from ray_NER import basic_clean, initModelDict, loadComposers, namedEntityRecognition
 
 ''' Parse initial tally tsv into slim composers tsv'''
 
@@ -13,20 +13,26 @@ def fix_nulls(s):
 def main(args):
     csv.field_size_limit(sys.maxsize)
     saveDel = args.save_deleted_in_csv == 'True'
+    parseTextNER = args.parse_text_fields == 'True'
+
+    if parseTextNER:
+        modelDict = initModelDict()
+        # composersDict,composersList = loadComposers(composer_dir)
 
     csv_dir = os.path.abspath(args.csv_file)
     slim_csv_dir = os.path.abspath('./slim_' + args.csv_file.lstrip('./'))
     if saveDel: deleted_csv_dir = os.path.abspath('./del_' + args.csv_file.lstrip('./'))
+    if parseTextNER: ner_csv_dir = os.path.abspath('./ner_' + args.csv_file.lstrip('./')); error_csv_dir =os.path.abspath('./ner_error_' + args.csv_file.lstrip('./'))
 
     #process large tsv line by line
     with open(csv_dir) as f_in, open(slim_csv_dir, "w+") as f_out:
         writer = csv.writer(f_out, delimiter=',', lineterminator='\n')
         reader = csv.reader(fix_nulls(f_in), delimiter=",")
-        if saveDel: f_out2 = open(deleted_csv_dir, "w+"); del_writer = csv.writer(f_out2, delimiter='\t', lineterminator='\n')        
+        if saveDel: f_out2 = open(deleted_csv_dir, "w+"); del_writer = csv.writer(f_out2, delimiter='\t', lineterminator='\n')  
+        if parseTextNER: f_out3 = open(ner_csv_dir,  "w+"); ner_writer = csv.writer(f_out3, delimiter='\t', lineterminator='\n');  f_out4 = open(error_csv_dir, "w+"); error_csv_writer = csv.writer(f_out4, delimiter='\t', lineterminator='\n');  
         for line in reader:
             ID,composers = line[0],line[3].split(';')
             cleanComposers = [basic_clean(composer) for composer in composers]
-            #remove duplicates
             #if no composers reamining post-clean, don't write?
             #write all good composers?
             if len(cleanComposers) > 0:
@@ -36,16 +42,32 @@ def main(args):
                     cleanComposers = '; '.join(cleanComposers)
                     writer.writerow([ID,cleanComposers])
                 else: 
+                    if parseTextNER: 
+                        title,desc = line[1].split(';'),line[2].split(';')
+                        titleEntities,descEntities = None,None
+                        if title:
+                            titleEntities = namedEntityRecognition(ID," ".join(title).lower(), modelDict ,  checkKnown=False, composersDict=None,composersList=None, error_csv_writer=error_csv_writer)
+                            if titleEntities: titleEntities = [basic_clean(x) for x in titleEntities if x]; 
+                            if titleEntities: titleEntities = "; ".join([x for x in titleEntities if x]).title()
+                            else: titleEntities = None
+                        if desc:
+                            descEntities = namedEntityRecognition(ID," ".join(desc).lower(), modelDict , checkKnown=False, composersDict=None,composersList=None, error_csv_writer=error_csv_writer)
+                            if descEntities: descEntities = [basic_clean(x) for x in descEntities if x]; 
+                            if descEntities: descEntities = "; ".join([x for x in descEntities if x]).title()
+                            else: descEntities = None
+                            
+                        ner_writer.writerow([ID,titleEntities,descEntities])
                     if saveDel: del_writer.writerow([ID,composers])
             else:
                 if saveDel: del_writer.writerow([ID,composers])
                 # print("No remaining composers for ID: " + str(ID) + ", Deleted: " + str(composers))
                 # del_writer.writerow([ID,[composer + " :" + basic_clean(composer,debug=True) for composer in composers]])
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""Process Metadata CSV.""")
-    parser.add_argument('-t', '--csv_file', default='./metadata.csv')
+    parser.add_argument('-c', '--csv_file', default='./metadata.csv')
     parser.add_argument('-d', '--save_deleted_in_csv', default='False')
+    parser.add_argument('-t', '--parse_text_fields', default='False')
+
     args = parser.parse_args()
     main(args)
--- data/ray_NER.py
+++ data/ray_NER.py
@@ -6,6 +6,101 @@ import ms3
 import ray
 import json
 from zipfile import ZipFile
+import spacy
+from langdetect import detect
+import csv
+
+#Language Settings
+#All lang models require install on target system with python -m spacy download language_core_model_name
+#SMALLMODEL uses a smaller spacy model, if false uses transformer or large model if available
+MULTIPLELANGUAGES = True
+ALTLANGS = {'de':["de_core_news_sm","de_dep_news_trf"], 'ko':["ko_core_news_sm","ko_core_news_lg"],'en':["en_core_web_sm","en_core_web_trf"],'ja':["ja_core_news_sm","ja_core_news_trf"],'ru':["ru_core_news_sm","ru_core_news_lg"],'it':["it_core_news_sm","it_core_news_lg"], 'es':["es_core_news_sm", "es_dep_new_trf"] }
+SMALLMODEL = True
+
+
+
+
+'''Draft Named Entity Recognition for retrieving possible composers from associated metadata'''
+
+#make composer list (composer,ID) and dict with ID -> [names]
+def loadComposers(file):
+    composersDict = dict()
+    composersList = list()
+    with open(file,'r') as comps:
+        for line in comps.readlines():
+            splitLine = line.split(',')
+            ID = splitLine[0].split('/')[-1]
+            composer = splitLine[1].strip('\n')
+            #TO DO - add stem flag to first instance of composer added to dict, all found composer instances of different languages for the same ID will be converted to the string of the canonical instance to reduce variation
+            if composersDict.get(ID):composersDict[ID] = composersDict[ID].append(composer)
+            else: composersDict[ID] = list(composer)
+            composersList.append((composer,ID))
+    return composersDict,composersList
+
+#initialize spacy models
+def initModelDict():
+    modelDict = dict()
+    #init model/s
+    if MULTIPLELANGUAGES:
+        for lang in ALTLANGS.keys():
+            if not SMALLMODEL:
+                modelDict[lang]=spacy.load(ALTLANGS[lang][1])
+            else: modelDict[lang]=spacy.load(ALTLANGS[lang][0])
+    else:
+        if not SMALLMODEL: modelDict['en'] = spacy.load("en_core_web_rtf") 
+        else: modelDict['en'] = spacy.load("en_core_web_sm")
+    return modelDict
+
+
+#used primarily for parsing B grade metadata fields
+def namedEntityRecognition(ID,stringToParse, modelDict , checkKnown=False, composersDict=None,composersList=None, error_csv_writer = None):
+    
+
+    
+    #Detect language - maybe useful feature / can be used to select correct model
+    try: lang = detect(stringToParse) 
+    except: 
+        print("failed to detect language of query " + stringToParse + " continuing with en")
+        #parse as english?
+        lang = 'en'
+
+    #if we have the language - try get the proper nouns
+    if modelDict.get(lang):
+
+        #remove special chars
+        stringToParse = ''.join(e for e in stringToParse if e.isalnum() or e==" " or e=="-")
+
+        #parse string with given model
+        doc = modelDict.get(lang)(stringToParse)
+        properNouns = [ent.text for ent in doc.ents ]
+
+    #if we dont have language - we cant recognize the proper nouns
+    else: 
+        # if SAVELANG: json.dump(jsonObj,jsonFile)
+        print("language model not available for query " + stringToParse.replace('\n', " ") + " in lang " + lang)
+        error_csv_writer.writerow([ID,lang, stringToParse.replace('\n', " ") ])
+
+        return None
+
+    #composerCheck
+    # if KNOWNCOMPOSERSONLY:
+    #     possibleComposers = knownComposerCheck(composersList,properNouns)
+    
+    # if DEBUG: print(possibleComposers)
+    #take best / most similar candidate for each proper noun
+    #Note: if not KNOWN composers only, then all proper nouns will be selected (due to similarity/distance measure used)
+    # candidates = list()
+    # for prop in properNouns:
+    #     candidates.append(sorted(possibleComposers, key= lambda x: lev_dist(prop,x[0]))[0])
+    
+    #convert to stem version of composer to reduce variation if possible
+    # if CONVERT_COMPOSER_TO_STEM:
+    #     if lang != 'en':
+    #         candidates = [ [enName for enName in getStemOfComposerName(candidate,composersDict)] for candidate in candidates]
+    # else:
+    #     candidates = [name[0] for name in candidates]
+    if properNouns: return [x for x in properNouns if x]
+    else: return None
 
 
 def basic_clean(composer: str, debug: bool = False) -> str:
@@ -46,10 +141,8 @@ def basic_clean(composer: str, debug: bool = False) -> str:
 
         composer = " ".join(composerStringList)
 
-
         composer = charStrip(" ',.- ", composer)
     
-        
         #remove double spaces
         composer = " ".join([x for x in composer.split(' ') if x])
 
