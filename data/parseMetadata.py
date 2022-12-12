e7058d8f52d396f2d256416f791cb65b24a79a8d
jmic8397 <jmic8397@uni.sydney.edu.au>
2022-12-12 14:51:52 +0100

    Added option to produce slim_metadata with both composer fields and NER fields

--- data/parseMetadata.py
+++ data/parseMetadata.py
@@ -14,6 +14,7 @@ def main(args):
     csv.field_size_limit(sys.maxsize)
     saveDel = args.save_deleted_in_csv == 'True'
     parseTextNER = args.parse_text_fields == 'True'
+    parseAll = args.parse_all_fields == 'True'
 
     if parseTextNER:
         modelDict = initModelDict()
@@ -21,6 +22,9 @@ def main(args):
 
     csv_dir = os.path.abspath(args.csv_file)
     slim_csv_dir = os.path.abspath('./slim_' + args.csv_file.lstrip('./'))
+    slim_all_csv_dir = os.path.abspath('./slim_all_fields_' + args.csv_file.lstrip('./'))
+    if parseAll: slim_csv_dir = slim_all_csv_dir
+
     if saveDel: deleted_csv_dir = os.path.abspath('./del_' + args.csv_file.lstrip('./'))
     if parseTextNER: ner_csv_dir = os.path.abspath('./ner_' + args.csv_file.lstrip('./')); error_csv_dir =os.path.abspath('./ner_error_' + args.csv_file.lstrip('./'))
 
@@ -29,7 +33,7 @@ def main(args):
         writer = csv.writer(f_out, delimiter=',', lineterminator='\n')
         reader = csv.reader(fix_nulls(f_in), delimiter=",")
         if saveDel: f_out2 = open(deleted_csv_dir, "w+"); del_writer = csv.writer(f_out2, delimiter='\t', lineterminator='\n')  
-        if parseTextNER: f_out3 = open(ner_csv_dir,  "w+"); ner_writer = csv.writer(f_out3, delimiter='\t', lineterminator='\n');  f_out4 = open(error_csv_dir, "w+"); error_csv_writer = csv.writer(f_out4, delimiter='\t', lineterminator='\n');  
+        if parseTextNER or parseAll: f_out3 = open(ner_csv_dir,  "w+"); ner_writer = csv.writer(f_out3, delimiter='\t', lineterminator='\n');  f_out4 = open(error_csv_dir, "w+"); error_csv_writer = csv.writer(f_out4, delimiter='\t', lineterminator='\n');  
         for line in reader:
             ID,composers = line[0],line[3].split(';')
             cleanComposers = [basic_clean(composer) for composer in composers]
@@ -41,22 +45,25 @@ def main(args):
                     cleanComposers = list(set(cleanComposers))
                     cleanComposers = '; '.join(cleanComposers)
                     writer.writerow([ID,cleanComposers])
+                    continue
                 else: 
-                    if parseTextNER: 
+                    if parseTextNER or parseAll: 
                         title,desc = line[1].split(';'),line[2].split(';')
                         titleEntities,descEntities = None,None
                         if title:
                             titleEntities = namedEntityRecognition(ID," ".join(title), modelDict ,  checkKnown=False, composersDict=None,composersList=None, error_csv_writer=error_csv_writer)
                             if titleEntities: titleEntities = [basic_clean(x, strict=True,initials=True) for x in titleEntities if x]; titleEntities = list(set(titleEntities))
                             if titleEntities: titleEntities = "; ".join([x for x in titleEntities if x]).title()
+                            if titleEntities and parseAll: writer.writerow([ID,titleEntities]); continue
                             else: titleEntities = None
                         if desc:
                             descEntities = namedEntityRecognition(ID," ".join(desc), modelDict , checkKnown=False, composersDict=None,composersList=None, error_csv_writer=error_csv_writer)
                             if descEntities: descEntities = [basic_clean(x, strict=True, initials=True) for x in descEntities if x]; descEntities = list(set(descEntities))
                             if descEntities: descEntities = "; ".join([x for x in descEntities if x]).title()
+                            if descEntities and parseAll: writer.writerow([ID,descEntities]); continue
                             else: descEntities = None
                             
-                        ner_writer.writerow([ID,titleEntities,descEntities])
+                        if not parseAll: ner_writer.writerow([ID,titleEntities,descEntities])
                     if saveDel: del_writer.writerow([ID,composers])
             else:
                 if saveDel: del_writer.writerow([ID,composers])
@@ -68,6 +75,6 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--csv_file', default='./metadata.csv')
     parser.add_argument('-d', '--save_deleted_in_csv', default='False')
     parser.add_argument('-t', '--parse_text_fields', default='False')
-
+    parser.add_argument('-a', '--parse_all_fields', default='False')
     args = parser.parse_args()
     main(args)
